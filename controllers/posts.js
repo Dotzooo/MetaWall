e428c7921f7edd79912f7bce4eb5d055c610255b
Adam Hsu <projectnba77@gmail.com>
2022-06-01 16:04:48 +0800

    fix: 1.取得所有貼文，關鍵字及排序搜尋條件 2.新增貼文，增加判斷有沒有這個使用者，避免傳入不存在的 id

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,17 +1,42 @@
+const mongoose = require('mongoose');
+
 const handleSuccess = require('../service/handleSuccess')
 const handleError = require('../service/handleError')
 
+const User = require('../model/user')
 const Posts = require('../model/post')
 
 const post = {
     async getPosts(req, res) {
-        const allPosts = await Posts.find()
+
+        const { query } = req
+
+        // 排序
+        const sort = query.sort === 'asc' ? 'createdAt' : '-createdAt'
+        // 關鍵字
+        const q = query.q !== undefined ? { "content": new RegExp(query.q.trim()) } : {}
+
+        const allPosts = await Posts.find(q).populate({
+            path: 'user',
+            select: 'name photoUrl'
+        }).sort(sort)
+
         handleSuccess(res, allPosts)
     },
-    async createPosts(req, res) {
+    async createPosts(req, res, next) {
         try {
             const { body } = req
-            
+            const isValid = mongoose.Types.ObjectId.isValid(body.userID)
+            if (!isValid) {
+                return handleError(res, '資料錯誤，請重新操作')
+            }
+
+            const isExist = await User.findById(body.userID)
+            if (!isExist) {
+                return handleError(res, '該用戶不存在，請重新操作')
+            }
+
+
             if (body.content) {
                 const newPost = await Posts.create({
                     user: body.userID ? body.userID : '628af3325625388ec1783221',
